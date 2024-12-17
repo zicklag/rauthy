27cf568c79710815a1b6eb0dd8a00b20615b8000
avdb13 <avdb@keemail.me>
2024-12-17 15:48:51 +0000

    blacklist upstream provider name

--- src/api/src/auth_providers.rs
+++ src/api/src/auth_providers.rs
@@ -70,6 +70,13 @@ pub async fn post_provider(
     principal.validate_admin_session()?;
     payload.validate()?;
 
+    if payload.name == "atproto" {
+        return Err(ErrorResponse::new(
+            ErrorResponseType::BadRequest,
+            "Must not contain a reserved name".to_string(),
+        ));
+    }
+
     if !payload.use_pkce && payload.client_secret.is_none() {
         return Err(ErrorResponse::new(
             ErrorResponseType::BadRequest,
@@ -261,6 +268,13 @@ pub async fn put_provider(
 ) -> Result<HttpResponse, ErrorResponse> {
     principal.validate_admin_session()?;
     payload.validate()?;
+
+    if payload.name == "atproto" {
+        return Err(ErrorResponse::new(
+            ErrorResponseType::BadRequest,
+            "Must not contain a reserved name".to_string(),
+        ));
+    }
 
     if !payload.use_pkce && payload.client_secret.is_none() {
         return Err(ErrorResponse::new(
